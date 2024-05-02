78c18b23a17ae1d7850bc1977019f3f7c48989a1
Hayden <110502238+hrocks6@users.noreply.github.com>
2024-05-02 17:26:15 -0400

    Initial code
    May try to optimize this at some point

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# mosers_powers_test
+# Mosers Powers Test
+
+MosersPowersVerifier was written to verify the results from [mosers_powers](https://github.com/Eiim/mosers_powers). It was written in Java to provide an independent implementation.
+
+The program receives an input file from the user and checks if the n and x (lines 1 and 2) satisfies the expression:
+
+    f(x) < 2^n < f(x+1)
+
+where
+
+    f(x) = 1/24x^4 - 1/4x^3 + 23/24x^2 -3/4x + 1
---
+++ src/MosersPowersVerifier.java
@@ -0,0 +1,150 @@
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+/**
+ * Program to verify mosers_powers results. Gets a file from user input and reads in 2 integers: n and x.
+ * Tests: f(x) < 2^n < f(x+1) where f(x)=1/24x⁴-1/4x³+23/24x²-3/4x+1
+ */
+public class MosersPowersVerifier
+{
+    // Static variables for f(x)
+    private static final BigDecimal OneTwentyfourth = BigDecimal.valueOf((double)1/24);
+    private static final BigDecimal TwentythreeTwentyfourth = BigDecimal.valueOf((double)23/24);
+    private static final BigDecimal OneFourth = new BigDecimal("0.25");
+    private static final BigDecimal ThreeFourths = new BigDecimal("0.75");
+    private static final BigDecimal One = new BigDecimal("1.0");
+
+    /**
+     * Gets a file from user input.
+     * @param scanner System.in scanner
+     * @return A file from user input
+     */
+    private static File getFile(Scanner scanner)
+    {
+        boolean exists = false;
+        File file;
+
+        // Loop until valid file is found.
+        do
+        {
+        System.out.print("Enter the name of the file:");
+        String filename = scanner.nextLine();
+        file = new File(filename);
+
+        if (!file.exists()) System.out.println("ERROR: File '" + filename + "' does not exist!");
+        else exists = true;
+
+        } while (!exists);
+
+        return file;
+    }
+
+    /**
+     * Returns 2^n
+     */
+    private static BigDecimal getPowerOfTwo(int n)
+    {
+        BigInteger powerOfTwo = new BigInteger("1");
+        powerOfTwo = powerOfTwo.shiftLeft(n);
+
+        //System.out.println(powerOfTwo);
+        return new BigDecimal(powerOfTwo.toString());
+    }
+
+    /**
+     * Calculates f(x)=1/24x⁴-1/4x³+23/24x²-3/4x+1
+     */
+    private static BigDecimal calculateFunction(BigInteger x)
+    {
+        BigDecimal xDecimal = new BigDecimal(x.toString());
+        BigDecimal result = new BigDecimal(x.toString());
+
+        // x(x(x(1/24x - 1/4) + 23/24) - 3/4) + 1
+        result = result.multiply(OneTwentyfourth);
+        result = result.subtract(OneFourth);
+        result = result.multiply(xDecimal);
+        result = result.add(TwentythreeTwentyfourth);
+        result = result.multiply(xDecimal);
+        result = result.subtract(ThreeFourths);
+        result = result.multiply(xDecimal);
+        result = result.add(One);
+
+        result = result.setScale(0, RoundingMode.HALF_UP);
+        //System.out.println(result);
+        return result;
+    }
+
+    /**
+     * Tests if f(x) < 2^n < f(x+1) where f(x)=1/24x⁴-1/4x³+23/24x²-3/4x+1
+     */
+    private static boolean verifyValues(BigInteger x, int n)
+    {
+        boolean valid = true;
+        System.out.println("Calculating 2^n...");
+        BigDecimal powerOfTwo = getPowerOfTwo(n);
+
+        System.out.println("Calculating f(x)...");
+        BigDecimal functionX = calculateFunction(x);
+        int compResult = functionX.compareTo(powerOfTwo);
+        switch (compResult) 
+        {
+            case 0: // Special output
+                System.out.println("f(x+1) = 2^n");
+                return false;
+            case 1: // f(x) >= 2^n, still need to check f(x+1) == 2^n
+                valid = false;
+        }
+
+        System.out.println("Calculating f(x+1)...");
+        BigDecimal functionXPlusOne = calculateFunction(x.add(new BigInteger("1")));
+        compResult = functionXPlusOne.compareTo(powerOfTwo);
+        switch (compResult) 
+        {
+            case 0: // Special output
+                System.out.println("f(x+1) = 2^n");
+            case -1: // f(x+1) <= 2^n
+                valid = false;
+        }
+
+        // Returns if conditions are met or not. Early return if f(x) = 2^n
+        return valid;
+    }
+
+    /**
+     * Asks the user for a file and then tests if f(x) < 2^n < f(x+1) where f(x)=1/24x⁴-1/4x³+23/24x²-3/4x+1
+     */
+    public static void main(String[] args)
+    {
+        BigInteger x = null;
+        int n = 0;
+
+        Scanner inputScanner = new Scanner(System.in), fileReader = null;
+        File file = getFile(inputScanner);
+        inputScanner.close();
+
+        try
+        {
+            fileReader = new Scanner(file);
+            n = Integer.parseInt(fileReader.nextLine());
+            System.out.println("N = " + n);
+            x = new BigInteger(fileReader.nextLine());
+            System.out.println("X = " + x + "\n");
+            fileReader.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("ERROR: " + e.getMessage());
+            System.exit(1);
+        }
+
+        boolean finalResult = verifyValues(x, n);
+
+        // Final output
+        System.out.println("\n---------------------------");
+        if (finalResult) System.out.println("f(x) < 2^n < f(x+1) = TRUE");
+        else System.out.println("f(x) < 2^n < f(x+1) = FALSE");
+    }
+}
